14c3c062dd7da201387a9b6a82d30e9c0c0e3ae9
Ondrej Prazak <oprazak@redhat.com>
2018-04-09 13:30:29 +0200

    Bench

--- db/migrate/20180403144853_convert_vm_attrs_to_hash.rb
+++ db/migrate/20180403144853_convert_vm_attrs_to_hash.rb
@@ -9,18 +9,36 @@ class ConvertVmAttrsToHash < ActiveRecord::Migration[5.1]
 
   def transform(transform_method)
     say "Starting serialized attributes conversion, this can take long time based on data amount"
-    say "Converting Nics"
+    say "Converting Nics, total: #{Nic::Base.unscoped.count}"
+    start = Time.now
     transform_batch_columns(Nic::Base, [:attrs, :compute_attributes], transform_method)
-    say "Converting Compute Attributes"
+    say "Took: #{Time.now - start}"
+
+    say "Converting Compute Attributes, total: #{ComputeAttribute.unscoped.count}"
+    start = Time.now
     transform_batch_columns(ComputeAttribute, [:vm_attrs], transform_method)
-    say "Converting Compute Resources"
+    say "Took: #{Time.now - start}"
+
+    say "Converting Compute Resources, total #{ComputeResource.unscoped.count}"
+    start = Time.now
     transform_batch_columns(ComputeResource, [:attrs], transform_method)
-    say "Converting Reports"
+    say "Took: #{Time.now - start}"
+
+    say "Converting Reports, total: #{Report.unscoped.count}"
+    start = Time.now
     transform_batch_columns(Report, [:metrics], transform_method)
-    say "Converting Lookup Keys"
+    say "Took: #{Time.now - start}"
+
+    say "Converting Lookup Keys, total: #{LookupKey.unscoped.count}"
+    start = Time.now
     transform_batch_columns(LookupKey, [:default_value], transform_method)
-    say "Converting Lookup Values"
+    say "Took: #{Time.now - start}"
+
+    say "Converting Lookup Values, total: #{LookupValue.unscoped.count}"
+    start = Time.now
     transform_batch_columns(LookupValue, [:value], transform_method)
+    say "Took: #{Time.now - start}"
+
     say "All conversions finished"
   end
 
