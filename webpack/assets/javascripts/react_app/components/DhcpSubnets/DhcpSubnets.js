afa15359467efdae8ca81ba7e39918e0a2167983
Ondrej Prazak <oprazak@redhat.com>
2018-04-23 13:18:21 +0200

    wip

--- webpack/assets/javascripts/react_app/components/DhcpSubnets/DhcpSubnets.js
+++ webpack/assets/javascripts/react_app/components/DhcpSubnets/DhcpSubnets.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { TabContainer, Nav, NavItem, TabContent, TabPane } from 'patternfly-react';
 
 class DhcpSubnets extends React.Component {
@@ -9,13 +8,25 @@ class DhcpSubnets extends React.Component {
 
   render() {
     return (
-      <div>
-        <ul className={"nav nav-tabs nav-tabs-pf nav-stacked col-md-3"}>
-          <li className={"active stacked-header"}>
-          </li>
+      // <div>
+      //   <ul className={"nav nav-tabs nav-tabs-pf nav-stacked col-md-3"}>
+      //     <li className={"active stacked-header"}>
+      //     </li>
 
-        </ul>
-      </div>
+      //   </ul>
+      // </div>
+      <TabContainer id='dhcp-subnets-tab-container' defaultActiveKey={0}>
+        <div>
+          <Nav bsClass='nav nav-tabs nav-tabs-pf nav-stacked col-md3'>
+            <NavItem className='stacked-header' eventKey={0}>General</NavItem>
+            <NavItem className='stacked-header' eventKey={1}>Dummy</NavItem>
+          </Nav>
+          <TabContent animation className='col-md-9 divided'>
+            <TabPane eventKey={0}>General info</TabPane>
+            <TabPane eventKey={1}>Dummy tab pane</TabPane>
+          </TabContent>
+        </div>
+      </TabContainer>
     )
   }
 }
