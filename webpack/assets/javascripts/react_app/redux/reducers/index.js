59f5b643cd21c0687fb3acdb196b0ef03c4786ff
Ondrej Prazak <oprazak@redhat.com>
2020-07-24 11:57:43 +0200

    Refs #29257 - Move state for edited setting

--- webpack/assets/javascripts/react_app/redux/reducers/index.js
+++ webpack/assets/javascripts/react_app/redux/reducers/index.js
@@ -22,7 +22,6 @@ import { reducers as modalReducers } from '../../components/ForemanModal';
 import { reducers as apiReducer } from '../API';
 import { reducers as modelsPageReducers } from '../../routes/Models/ModelsPage';
 import { reducers as settingRecordsReducers } from '../../components/SettingRecords';
-import { reducers as settingsTableReducers } from '../../components/SettingsTable';
 
 export function combineReducersAsync(asyncReducers) {
   return combineReducers({
@@ -41,7 +40,6 @@ export function combineReducersAsync(asyncReducers) {
     ...factChartReducers,
     ...typeAheadSelectReducers,
     ...settingRecordsReducers,
-    ...settingsTableReducers,
 
     router: connectRouter(history),
     // Pages
